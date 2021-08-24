d63f5097e8198472584fc0d7902eff33c039b730
Nikolay Sitnikov <nicholasitnikov@gmail.com>
2021-08-24 19:13:13 +0500

    Проверка пемернной

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 
     const { id } = req.query
     const baseUrl = process.env.NODE_ENV === 'production' ? 'https://econ.store' : 'http://localhost:3000';
-    let productsResponse = await axios.get(`${baseURl}/db.json`);
+    let productsResponse = await axios.get(`${baseUrl}/db.json`);
     res.status(200).json(productsResponse.data.filter((el) => el.id === parseInt(id))[0] || null);
 
 }
