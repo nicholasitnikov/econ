3fbcccf21653078617f6901db91d720d9f9e51ff
Nikolay Sitnikov <nicholasitnikov@gmail.com>
2021-08-24 19:14:43 +0500

    Обновление переменной

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -4,7 +4,6 @@ export default async function handler(req, res) {
 
     const baseUrl = process.env.NODE_ENV === 'production' ? 'https://econ.store' : 'http://localhost:3000';
     let productsResponse = await axios.get(`${baseUrl}/db.json`);
-    res.status(200).json(process.env.NODE_ENV);
-    // res.status(200).json(productsResponse.data);
+    res.status(200).json(productsResponse.data);
 
 }
